23fafa13a8289b5c91d1cf1da2467e01c965d398
NAVEENRAJ2004 <naveenraj10203040@gmail.com>
2025-06-20 00:53:19 +0530

    added singleton pattern solution in week 1 directory

---
+++ Week 1/Design principles & Patterns in C#/SingletonPatternExample/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SingletonPatternExample
+{
+    internal class Logger
+    {
+        private static Logger? instance = null; // Private Instance set null at initial stage
+        private static readonly object lockObj = new object(); // lock object for thread safety
+
+        // Private constructor
+        private Logger() {
+            Console.WriteLine("Logger initialized.");
+        }
+
+        public static Logger GetInstance()
+        {
+            if(instance == null)
+            {
+                lock(lockObj)
+                {
+                    if(instance == null)
+                    {
+                        instance = new Logger();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine("Log : {0}", message);
+        }
+    }
+}
---
+++ Week 1/Design principles & Patterns in C#/SingletonPatternExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace SingletonPatternExample
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Logger L1 = Logger.GetInstance();
+            L1.Log("Message from Instance 1");
+
+            Logger L2 = Logger.GetInstance();
+            L2.Log("Message from Instance 2");
+
+            Console.WriteLine($"Are Both Instance are Same? : {(L1 == L2 ? "Yes": "No")}");
+        }
+    }
+}
